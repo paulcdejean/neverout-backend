21ba4860c9c19b0cca07ebf26143ffaf66162fab
Paul Dejean <paul@pauldejean.com>
2016-02-21 12:08:53 -0600

    Oh yeah and it needs a url too.

--- neverout/urls.py
+++ neverout/urls.py
@@ -6,7 +6,9 @@ admin.autodiscover()
 import backend.views
 
 urlpatterns = [
-    url(r'^add_point/([-+]?[0-9]*\.?[0-9]+)$', backend.views.add_point, name='add_point'),
+    url(r"^add_point/([-+]?[0-9]*\.?[0-9]+)$", backend.views.add_point, name="add_point"),
+    url(r"set_empty/([-+]?[0-9]*\.?[0-9]+)$", backend.views.set_empty, name="set_empty"),
+    url(r"set_full/([-+]?[0-9]*\.?[0-9]+)$", backend.views.set_empty, name="set_full"),
     url(r'^$', backend.views.index, name='index'),
     url(r'^name$', backend.views.name, name='name'),
     url(r'^admin/', include(admin.site.urls)),
