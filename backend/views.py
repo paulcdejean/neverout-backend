d24229772c23dbb2dd05c9f9bce23271e394b5f0
Paul Dejean <paul@pauldejean.com>
2016-02-21 03:25:00 -0600

    Trying to write to the database.

--- backend/views.py
+++ backend/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from backend.models import *
+
 def add_point(request, value):
-    return HttpResponse("Hello, world. You're at the polls index." + value)
+    new_reading = Reading(3.14)
+    new_reading.save()
+    return HttpResponse(Reading.objects.all())
