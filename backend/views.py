643b69c17089f9019da610620b716aa29a692449
Paul Dejean <paul@pauldejean.com>
2016-02-21 03:09:50 -0600

    Attempted to add a view.

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def add_point(request, value):
+    return HttpResponse("Hello, world. You're at the polls index." + value)
--- neverout/urls.py
+++ neverout/urls.py
@@ -6,7 +6,6 @@ admin.autodiscover()
 import backend.views
 
 urlpatterns = [
-    url(r'^$', backend.views.index, name='index'),
-    url(r'^db', backend.views.db, name='db'),
+    url(r'^add_point/([-+]?[0-9]*\.?[0-9]+)$', backend.views.add_point, name='add_point'),
     url(r'^admin/', include(admin.site.urls)),
 ]
